af30dbcacda74c44f065e11087a59a889936c00c
Patricia Salazar <patricia.escorche@gmail.com>
2020-09-02 00:46:33 -0400

    Add develop folder

---
+++ Develop/routes/html-routes.js
@@ -0,0 +1,26 @@
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+
+};
